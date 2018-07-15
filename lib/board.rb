db08cf6791845ac7c0daafd14414f47feaa77d28
Patrick Shobe <patrick@patrickshobe.com>
2018-07-15 15:30:30 -0600

    Finds horizontal and vertical wins

--- lib/board.rb
+++ lib/board.rb
@@ -27,4 +27,39 @@ class Board
       move(piece, column, (row - 1))
     end
   end
+
+  def win_check
+    winner = ''
+
+    # Lateral Check
+    @board.each do |row|
+      if row.values.join.include?("XXXX")
+        winner = 'Computer'
+        break
+      elsif row.values.join.include?("OOOO")
+        winner = 'Player'
+        break
+      end
+    end
+
+    # Vertical Check
+
+    columns = %I[A B C D E F G]
+
+    columns.each do |column|
+      value_array = []
+      (0..5).to_a.each do |row|
+        value_array << @board[row][column]
+      end
+
+      if value_array.join.include?("XXXX")
+        winner = 'Computer'
+        break
+      elsif value_array.join.include?("OOOO")
+        winner = 'Player'
+        break
+      end
+    end
+    winner
+  end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -71,4 +71,33 @@ class BoardTest < Minitest::Test
 
     assert_equal expected, board.board
   end
+
+  def test_it_finds_horizontal_wins
+    board = Board.new
+    piece = Piece.new(:X)
+    piece1 = Piece.new(:X)
+    piece2 = Piece.new(:X)
+    piece3 = Piece.new(:X)
+    board.move(piece, :A)
+    board.move(piece1, :B)
+    board.move(piece2, :C)
+    board.move(piece3, :D)
+    assert_equal 'Computer', board.win_check
+  end
+
+  def test_it_finds_vertical_wins
+    board = Board.new
+    piece = Piece.new(:X)
+    piece1 = Piece.new(:X)
+    piece2 = Piece.new(:X)
+    piece3 = Piece.new(:X)
+    board.move(piece, :G)
+    board.move(piece1, :G)
+    board.move(piece2, :G)
+    board.move(piece3, :G)
+    assert_equal 'Computer', board.win_check
+  end
+
+
+
 end
