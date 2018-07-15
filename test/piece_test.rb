2f93be012341abdd8d7615eb55479a5cd818f544
Patrick Shobe <patrick@patrickshobe.com>
2018-07-15 13:09:49 -0600

    Adds Piece Tests

---
+++ test/piece_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/emoji'
+require './lib/piece'
+
+class PieceTest < Minitest::Test
+  def test_it_exists
+    piece = Piece.new("X")
+
+    assert_instance_of Piece, piece
+  end
+
+  def tets_it_holds_a_value
+    piece = Piece.new("X")
+
+    assert_equal "X", piece.value
+  end
+end
