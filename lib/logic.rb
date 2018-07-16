d0213ab208dd02a8f94a198c03aaad879a6ad15e
Patrick Shobe <patrick@patrickshobe.com>
2018-07-16 14:26:06 -0600

    Logic cleaned up

--- lib/logic.rb
+++ lib/logic.rb
@@ -5,21 +5,20 @@ require './lib/board'
 
 # ./lib/logic
 class Logic
-  def initialize(game_board)
-    @game_board = game_board.game_board
-    @array = []
+  def initialize(board_object)
+    @game_board = board_object.game_board
     @winner = nil
   end
 
   def check_for_win
-    horizontal_win_check
-    vertical_win_check
-    right_check
-    left_check
+    check_for_horizontal_wins
+    check_for_vertical_wins
+    check_for_right_angle_wins
+    check_for_left_angle_wins
     @winner
   end
 
-  def horizontal_win_check
+  def check_for_horizontal_wins
     @game_board.each do |row|
       @winner = checker(row)
       break unless @winner.nil?
@@ -27,7 +26,7 @@ class Logic
     @winner
   end
 
-  def vertical_win_check
+  def check_for_vertical_wins
     (0..6).each do |column|
       value_array = []
       (0..5).to_a.each { |row| value_array << @game_board[row][column] }
@@ -37,7 +36,7 @@ class Logic
     @winner
   end
 
-  def right_check
+  def check_for_right_angle_wins
     starting_points = [[5, 3], [5, 2], [5, 1], [5, 0], [4, 0], [3, 0]]
     starting_points.each do |point|
       @winner = right_angle_join(point)
@@ -46,7 +45,7 @@ class Logic
     @winner
   end
 
-  def left_check
+  def check_for_left_angle_wins
     starting_points = [[5, 3], [5, 4], [5, 5], [5, 6], [4, 6], [3, 6]]
     starting_points.each do |point|
       @winner = left_angle_join(point)
@@ -55,24 +54,24 @@ class Logic
     @winner
   end
 
-  def right_angle_join(starting_point = [5, 3])
+  def right_angle_join(starting_point = [5, 3], array = [])
     row = starting_point[0]
     column = starting_point[1]
     unless @game_board[row][column].nil?
-      @array << @game_board[row][column]
-      right_angle_join([(row - 1), (column + 1)])
+      array << @game_board[row][column]
+      right_angle_join([(row - 1), (column + 1)], array)
     end
-    checker(@array)
+    checker(array)
   end
 
-  def left_angle_join(starting_point = [5, 3])
+  def left_angle_join(starting_point = [5, 3], array = [])
     row = starting_point[0]
     column = starting_point[1]
     unless @game_board[row][column].nil?
-      @array << @game_board[row][column]
-      left_angle_join([(row - 1), (column - 1)])
+      array << @game_board[row][column]
+      left_angle_join([(row - 1), (column - 1)], array)
     end
-    checker(@array)
+    checker(array)
   end
 
   def checker(array)
